b75aca5ee0c671f7ecd0b32f695cd8a0b049211b
DannyKaschub <dannykashub98@gmail.com>
2023-03-26 19:39:58 +0200

    erste version vom header sowie dem switch zwichen den seiten

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +8,37 @@
     <title>Einkaufsliste</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+    <header>
+        <div>Einkaufsliste</div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="?page=add">Hinzufügen</a>
+                    <a href="?page=list">Akruelle Liste</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <div>
+        <?php
+            $headline = 'Einkaufsliste';
+            #anzeige der hinzufügen seite
+            if($_GET['page'] == 'add'){
+                $headline ='Hier kannst du neue Sachen einfügen';
+            }
+            #anzeige der liste
+            if($_GET['page'] == 'list'){
+                $headline ='hier ist die liste deiner sachen';
+            }
+
+            echo "<h1>$headline</h1>";
+        ?>
+    </div>
+
 </body>
+
 </html>
