e21e1b21469d3d5c4661a05b953472b41c42c185
DannyKaschub <dannykashub98@gmail.com>
2023-03-26 19:57:59 +0200

    erstes layout für die 'hinzufügen' seite.

--- index.php
+++ index.php
@@ -25,17 +25,24 @@
 
     <div>
         <?php
-            $headline = 'Einkaufsliste';
-            #anzeige der hinzufügen seite
-            if($_GET['page'] == 'add'){
-                $headline ='Hier kannst du neue Sachen einfügen';
-            }
-            #anzeige der liste
-            if($_GET['page'] == 'list'){
-                $headline ='hier ist die liste deiner sachen';
-            }
 
-            echo "<h1>$headline</h1>";
+        #anzeige der hinzufügen seite
+        if ($_GET['page'] == 'add') {
+            echo "
+                <form action='?page=list' method='POST'>
+                    <div class='input_container'>
+                        <input required type='text' placeholder='Was wird benötigt'>
+                        <input type='text' placeholder='Wie viel soll es sein.(optional)'>
+                        <input type='text' placeholder='kategorie'>
+                        <button type='submit'>Hinzufügen</button>
+                    </div>
+                </form>
+                ";
+        }
+        #anzeige der liste
+        if ($_GET['page'] == 'list') {
+        }
+
         ?>
     </div>
 
