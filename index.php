4ab3b4f25478743b4ea587aac7dfaa9146a8ae86
DannyKaschub <dannykashub98@gmail.com>
2023-03-28 21:07:19 +0200

    hinzufügen der logik um die eingegebenen daten visuel anzuzeigen

--- index.php
+++ index.php
@@ -25,22 +25,52 @@
 
     <div>
         <?php
+        $produkte = [];
 
         #anzeige der hinzufügen seite
         if ($_GET['page'] == 'add') {
             echo "
                 <form action='?page=list' method='POST'>
                     <div class='input_container'>
-                        <input required type='text' placeholder='Was wird benötigt'>
-                        <input type='text' placeholder='Wie viel soll es sein.(optional)'>
-                        <input type='text' placeholder='kategorie'>
+                        <input name='produkt' required type='text' placeholder='Was wird benötigt'>
+                        <input name='menge' type='text' placeholder='Wie viel soll es sein.(optional)'>
                         <button type='submit'>Hinzufügen</button>
                     </div>
                 </form>
                 ";
         }
         #anzeige der liste
+        
+        #läd die produkte.txt wenn sie existiert und erstellt ein json
+        if (file_exists('produkte.txt')) {
+            $text = file_get_contents('produkte.txt', true);
+            $products = json_decode($text, true);
+        }
+
+        #fügt ein neues produkt ins produkte array ein und speichert es wieder als txt.
+        if (isset($_POST['produkt'])) {
+            $newProduct = [
+                'produkt' => $_POST['produkt'],
+                'menge' => $_POST['menge']
+            ];
+            array_push($products, $newProduct);
+            file_put_contents('produkte.txt', json_encode($products, JSON_PRETTY_PRINT));
+        }
+
+
         if ($_GET['page'] == 'list') {
+
+            foreach ($products as $row) {
+                $produkt = $row['produkt'];
+                $menge = $row['menge'];
+                echo "
+                    <div class='card'>
+                        <span>$menge</span>
+                        <span>$produkt</span>
+                        <input type='checkbox'>
+                    </div>
+                ";
+            }
         }
 
         ?>
